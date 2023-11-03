8ea1475cce5745f639c3a62ec0b3a566957adeaf
rad-li <74559754+rad-li@users.noreply.github.com>
2023-11-03 19:37:47 +0300

    Add files via upload

---
+++ yandex-speech-to-text.py
@@ -0,0 +1,16 @@
+"""
+Скрипт для озвучивания введенного текста
+Используется синтезатор речи Яндекса. Для проигрывания потока нужен mplayer.
+Можно заменить на свою программу. Для запуска: python yandex-speech-to-text.py
+"""
+
+import subprocess
+
+
+url = 'http://tts.voicetech.yandex.net/generate?'
+
+# текст для озвучивания
+text = "привет"
+
+# получаем звук по ссылке и проигрываем с помощью mplayer
+subprocess.run(["mplayer", "-really-quiet", "-nolirc", 'http://tts.voicetech.yandex.net/tts?format=mp3&quality=hi&lang=ru_RU&text='+text])
